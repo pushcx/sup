7980865fc05242bee592ab97d9a018af089c7973
Rich Lane <rlane@club.cc.cmu.edu>
2009-09-15 20:46:41 -0700

    save label changes immediately

--- lib/sup/modes/inbox-mode.rb
+++ lib/sup/modes/inbox-mode.rb
@@ -36,11 +36,13 @@ class InboxMode < ThreadIndexMode
 
     UndoManager.register "archiving thread" do
       thread.apply_label :inbox
+      save_thread_state thread
       add_or_unhide thread.first
     end
 
-    cursor_thread.remove_label :inbox
-    hide_thread cursor_thread
+    thread.remove_label :inbox
+    save_thread_state thread
+    hide_thread thread
     regen_text
   end
 
@@ -48,6 +50,7 @@ class InboxMode < ThreadIndexMode
     UndoManager.register "archiving #{threads.size.pluralize 'thread'}" do
       threads.map do |t|
         t.apply_label :inbox
+        save_thread_state t
         add_or_unhide t.first
       end
       regen_text
@@ -55,6 +58,7 @@ class InboxMode < ThreadIndexMode
 
     threads.each do |t|
       t.remove_label :inbox
+      save_thread_state t
       hide_thread t
     end
     regen_text
@@ -67,12 +71,14 @@ class InboxMode < ThreadIndexMode
     UndoManager.register "reading and archiving thread" do
       thread.apply_label :inbox
       thread.apply_label :unread
+      save_thread_state thread
       add_or_unhide thread.first
     end
 
-    cursor_thread.remove_label :unread
-    cursor_thread.remove_label :inbox
-    hide_thread cursor_thread
+    thread.remove_label :unread
+    thread.remove_label :inbox
+    save_thread_state thread
+    hide_thread thread
     regen_text
   end
 
@@ -82,6 +88,7 @@ class InboxMode < ThreadIndexMode
     threads.each do |t|
       t.remove_label :unread
       t.remove_label :inbox
+      save_thread_state t
       hide_thread t
     end
     regen_text
@@ -89,6 +96,7 @@ class InboxMode < ThreadIndexMode
     UndoManager.register "reading and archiving #{threads.size.pluralize 'thread'}" do
       threads.zip(old_labels).each do |t, l|
         t.labels = l
+        save_thread_state t
         add_or_unhide t.first
       end
       regen_text
--- lib/sup/modes/thread-view-mode.rb
+++ lib/sup/modes/thread-view-mode.rb
@@ -132,7 +132,12 @@ EOS
     @layout[earliest].state = :detailed if earliest.has_label?(:unread) || @thread.size == 1
 
     @thread.remove_label :unread
+    save_state
     regen_text
+  end
+
+  def save_state
+    @thread.save_state Index
   end
 
   def draw_line ln, opts={}
@@ -260,6 +265,7 @@ EOS
 
     return unless new_labels
     @thread.labels = Set.new(reserved_labels) + new_labels
+    save_state
     new_labels.each { |l| LabelManager << l }
     update
     UpdateManager.relay self, :labeled, @thread.first
@@ -278,8 +284,10 @@ EOS
   def toggle_label m, label
     if m.has_label? label
       m.remove_label label
+      save_state
     else
       m.add_label label
+      save_state
     end
     ## TODO: don't recalculate EVERYTHING just to add a stupid little
     ## star to the display
@@ -502,6 +510,7 @@ EOS
   def archive_and_then op
     dispatch op do
       @thread.remove_label :inbox
+      save_state
       UpdateManager.relay self, :archived, @thread.first
     end
   end
@@ -509,6 +518,7 @@ EOS
   def spam_and_then op
     dispatch op do
       @thread.apply_label :spam
+      save_state
       UpdateManager.relay self, :spammed, @thread.first
     end
   end
@@ -516,6 +526,7 @@ EOS
   def delete_and_then op
     dispatch op do
       @thread.apply_label :deleted
+      save_state
       UpdateManager.relay self, :deleted, @thread.first
     end
   end
@@ -523,6 +534,7 @@ EOS
   def unread_and_then op
     dispatch op do
       @thread.apply_label :unread
+      save_state
       UpdateManager.relay self, :unread, @thread.first
     end
   end
