1a13738734b5a5c3276264b3478739bc916f8381
Rich Lane <rlane@club.cc.cmu.edu>
2009-09-19 16:26:51 -0700

    remove old code from InboxMode

--- lib/sup/modes/inbox-mode.rb
+++ lib/sup/modes/inbox-mode.rb
@@ -29,54 +29,25 @@ class InboxMode < ThreadIndexMode
   def killable?; false; end
 
   def archive
-    return unless cursor_thread
-    thread = cursor_thread # to make sure lambda only knows about 'old' cursor_thread
-
-    UndoManager.register "archiving thread" do
-      thread.apply_label :inbox
-      save_thread_state thread
-      add_or_unhide thread.first
-    end
-
+    return unless thread = cursor_thread
     thread.remove_label :inbox
     save_thread_state thread
-    hide_thread thread
     regen_text
   end
 
   def multi_archive threads
-    UndoManager.register "archiving #{threads.size.pluralize 'thread'}" do
-      threads.map do |t|
-        t.apply_label :inbox
-        save_thread_state t
-        add_or_unhide t.first
-      end
-      regen_text
-    end
-
     threads.each do |t|
       t.remove_label :inbox
       save_thread_state t
-      hide_thread t
     end
     regen_text
   end
 
   def read_and_archive
-    return unless cursor_thread
-    thread = cursor_thread # to make sure lambda only knows about 'old' cursor_thread
-
-    UndoManager.register "reading and archiving thread" do
-      thread.apply_label :inbox
-      thread.apply_label :unread
-      save_thread_state thread
-      add_or_unhide thread.first
-    end
-
+    return unless thread = cursor_thread
     thread.remove_label :unread
     thread.remove_label :inbox
     save_thread_state thread
-    hide_thread thread
     regen_text
   end
 
@@ -87,19 +58,8 @@ class InboxMode < ThreadIndexMode
       t.remove_label :unread
       t.remove_label :inbox
       save_thread_state t
-      hide_thread t
     end
     regen_text
-
-    UndoManager.register "reading and archiving #{threads.size.pluralize 'thread'}" do
-      threads.zip(old_labels).each do |t, l|
-        t.labels = l
-        save_thread_state t
-        add_or_unhide t.first
-      end
-      regen_text
-    end
-
   end
 
   def handle_unarchived_update sender, m
@@ -108,7 +68,6 @@ class InboxMode < ThreadIndexMode
 
   def handle_archived_update sender, m
     t = thread_containing(m) or return
-    hide_thread t
     regen_text
   end
 
